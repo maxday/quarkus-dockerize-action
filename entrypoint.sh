8bac2b693883617e3bc69b69e6eb58398642d86f
Maxime DAVID <contact@maximedavid.fr>
2020-03-20 19:46:52 -0400

    params entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 
 set -e
-TAG=$1
+
+EXECUTABLE_PATH=1
+TAG=$2
+DOCKERFILE_PATH=$3
+DOCKER_FILE_TO_USE="/Dockerfile"
+
+if [ -z $EXECUTABLE_PATH ]; then
+  echo '<executablePath> parameter is missing'
+  exit 1
+fi
 
 if [ -z $TAG ]; then
   echo '<tag> parameter is missing'
   exit 1
 fi
 
-chmod u+x native-executable/app-runner
-docker build . -f /Dockerfile.dockerize -t $TAG
+if [ -n "$DOCKERFILE_PATH" ]; then
+  if [ -f $DOCKERFILE_PATH ] ; then
+    echo "Dockerfile found"
+    DOCKER_FILE_TO_USE=$DOCKERFILE_PATH
+  else
+    echo "Imposible to find Dockerfile at path : $DOCKERFILE_PATH"
+    exit 1
+  fi
+fi
+
+docker build . -f $DOCKER_FILE_TO_USE -t native-build-image
+
+chmod u+x $EXECUTABLE_PATH
+docker build . -f $DOCKER_FILE_TO_USE -t $TAG
 
 echo ::set-output name=image::$TAG
